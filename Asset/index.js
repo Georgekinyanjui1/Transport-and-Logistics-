b663664de948cc5e56e3a6d34293323907e8db2c
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-25 23:13:16 +0300

    defined variables

---
+++ Asset/index.js
@@ -0,0 +1,7 @@
+const myName = document.getElementById("name");
+const contact = document.getElementById("number");
+const email = document.getElementById("email");
+const place = document.getElementById("place");
+const button = document.getElementById("button");
+const form = document.getElementById("form")
+const error = document.getElementById("error")
