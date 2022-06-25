7ab0ae8f1ca579943574a4882bd395ee32e6bba7
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-25 23:15:03 +0300

    created function

--- Asset/index.js
+++ Asset/index.js
@@ -5,3 +5,24 @@ const place = document.getElementById("place");
 const button = document.getElementById("button");
 const form = document.getElementById("form")
 const error = document.getElementById("error")
+form.addEventListener("submit", (e) => {
+
+    if (myName.value === '' || myName.value == null) {
+        alert("Name is required")
+    } else {
+        if (contact.value.length === 10) {
+            alert("please confirm your number")
+        } else {
+            if (email.value === "" || email.value == null) {
+                alert("check your email to confirm")
+            } else {
+                if (place.value === "" || place.value == null) {
+                    alert("invalid")
+                }
+            }
+
+        }
+
+
+    }
+})
